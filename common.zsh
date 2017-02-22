c403d076e3561ff39246353c262da330915c5fb8
isuke <isuke770@gmail.com>
2017-02-22 22:40:02 +0900

    add setopt to common.zsh

--- common.zsh
+++ common.zsh
@@ -1,7 +1,11 @@
-### Path ###
+#
+# Path
+#
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-### Commands ###
+#
+# Commands
+#
 # history
 alias h='history'
 
@@ -17,3 +21,18 @@ alias less='less -N -S -#4'
 
 # sublime
 alias sublime_package="~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
+
+#
+# 補完
+#
+setopt auto_param_slash      # ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt mark_dirs             # ファイル名の展開でディレクトリにマッチした場合 末尾に / を付加
+setopt auto_param_keys       # カッコの対応などを自動的に補完
+setopt complete_in_word      # 語の途中でもカーソル位置で補完
+setopt always_last_prompt    # カーソル位置は保持したままファイル名一覧を順次その場で表示
+setopt print_eight_bit       # 日本語ファイル名等8ビットを通す
+setopt extended_glob         # 拡張グロブで補完(~とか^とか。例えばless *.txt~memo.txt ならmemo.txt 以外の *.txt にマッチ)
+setopt globdots              # 明確なドットの指定なしで.から始まるファイルをマッチ
+
+zstyle ':completion:*:default' menu select=2
+zstyle ':completion:*:*files' ignored-patterns '*?.o' '*?~' '*\#'
