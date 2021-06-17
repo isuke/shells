e07144d7c6f6f05a54a9e60434e4064729b924a2
isuke <isuke770@gmail.com>
2021-06-17 17:01:09 +0900

    Remove 'rails_new_vue' alias

--- ruby.zsh
+++ ruby.zsh
@@ -6,4 +6,3 @@ alias bes="bundle exec spring"
 
 # Rails
 alias rails_new='bundle exec rails new .     --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-sprockets --skip-javascript --skip-turbolinks --skip-test --skip-system-test --skip-bundle --skip-webpack-install'
-alias rails_new_vue='bundle exec rails new . --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable                                    --skip-turbolinks --skip-test --skip-system-test --skip-bundle --webpack=vue'
