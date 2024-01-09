44b3286b92ccc09c193c3a53be1b9d95cce4cd7b
isuke <isuke770@gmail.com>
2024-01-09 12:01:41 +0900

    Fix irb alias for old ruby

--- ruby.zsh
+++ ruby.zsh
@@ -5,7 +5,15 @@ alias be="bundle exec"
 alias bes="bundle exec spring"
 
 # irb
-alias irb='irb --no-pager --type-completor --prompt-mode=simple'
+function irb_with_options () {
+  if [[ $(ruby -v) =~ "ruby 3\.[2..9]" ]]; then
+    \irb --prompt-mode=simple --no-pager --type-completor
+  else
+    \irb --prompt-mode=simple
+  fi
+}
+
+alias irb=irb_with_options
 
 # Rails
 alias rails_new='bundle exec rails new .     --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-sprockets --skip-javascript --skip-turbolinks --skip-test --skip-system-test --skip-bundle --skip-webpack-install'
